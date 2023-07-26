680db5a5c826ec5b9f7741836caebe295805a327
Tallysson2k23 <139917065+Tallysson2k23@users.noreply.github.com>
2023-07-26 09:01:49 -0300

    exercicio 10

---
+++ exercicio10.js
@@ -0,0 +1,8 @@
+//EX: 10
+
+    const prompt = require("prompt-sync")();
+        var celsius = parseFloat (prompt ("Informa a temperatura em Celsius: "));
+        var fahrenheit = (9*celsius/5) + 32 ;
+            console.log("Temperatura em graus Fahrenheit = ", 
+            fahrenheit);
+    
